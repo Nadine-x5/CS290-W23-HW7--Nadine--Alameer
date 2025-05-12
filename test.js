dce862fa1337ffa7dc03faad9613c79e991a04ee
LayanMohammedAlqabbani <154751429+LayanMohammedAlqabbani@users.noreply.github.com>
2025-05-13 00:48:42 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+  let res = http.get('https://example.com');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
