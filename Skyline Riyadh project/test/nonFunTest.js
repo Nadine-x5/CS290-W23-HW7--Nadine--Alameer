8438a534e77b23062dd61286587f016fe2b3fdaf
Nadine-x5 <nadeenalameer5@gmail.com>
2025-05-12 13:29:51 +0300

    delete

--- Skyline Riyadh project/test/nonFunTest.js
+++
@@ -1,9 +0,0 @@
-// tests/non-functional/performance.test.js
-const { validatePassword } = require('../../src/passwordValidator');
-
-test('Validation should be fast', () => {
-  const start = Date.now();
-  validatePassword('V3ryL0ngP@ssw0rd!!');
-  const timeTaken = Date.now() - start;
-  expect(timeTaken).toBeLessThan(10); // should take less than 10ms
-});
--- Skyline Riyadh project/test/password.test.js
+++
@@ -1,6 +0,0 @@
-// tests/password.test.js
-const { validatePassword } = require('../src/passwordValidator');
-
-test('Dummy test - remove later', () => {
-  expect(1 + 1).toBe(2); // This will always pass
-});
